50c6aa493485a2d9e46cbb59fc237d723e171ed0
lakithjayalath <lakith54321@gmail.com>
2021-10-13 00:57:22 +0530

    PDF displayed in WKWebView

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "test", ofType: "pdf")
+        let url = URL(fileURLWithPath: path!)
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
     }
 
 
